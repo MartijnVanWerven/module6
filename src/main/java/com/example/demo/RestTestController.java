c578667905d1daf3562dd3d8535e50320d4aca70
Gerben <gbvanoene@gmail.com>
2022-02-08 19:25:04 +0100

    Voegt temperatuur repository toe

--- src/main/java/com/example/demo/RestTestController.java
+++ src/main/java/com/example/demo/RestTestController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.entities.Temperatuur;
+import com.example.demo.entities.TemperatuurRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestTestController {
+
+    private final TemperatuurRepository repository;
 
     RestTestController(TemperatuurRepository repository) {
         this.repository = repository;
