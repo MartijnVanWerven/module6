45b3e12b9895ca3a3bb89168c79ad78e8b00be1f
Gerben <gbvanoene@gmail.com>
2022-02-08 19:17:30 +0100

    Voegt Temperatuur PUT toe

--- src/main/java/com/example/demo/RestTestController.java
+++ src/main/java/com/example/demo/RestTestController.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.entities.Temperatuur;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestTestController {
 
-    RestTestController() {
+    RestTestController(TemperatuurRepository repository) {
+        this.repository = repository;
 
     }
 
@@ -14,4 +19,18 @@ public class RestTestController {
         return "YAY!!";
     }
 
+
+    @PutMapping("/temperatuur/{id}")
+    Temperatuur replaceEmployee(@RequestBody Temperatuur newTemperatuur, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    temperatuur.setTemperatuur(newTemperatuur.getName());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newTemperatuur.setId(id);
+                    return repository.save(newTemperatuur);
+                });
+    }
 }
