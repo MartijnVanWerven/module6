2a1a8af807dd1844c8c03777a1c418e08228771c
MartijnVanWerven <martijn@mvanwerven.nl>
2022-02-08 19:27:41 +0100

    temperatuur

--- src/main/java/com/example/demo/entities/Temperatuur.java
+++ src/main/java/com/example/demo/entities/Temperatuur.java
@@ -1,14 +1,47 @@
 package com.example.demo.entities;
 
 import java.time.Instant;
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Temperatuur {
-    final Instant meetTijdStip;
-    final Double temperatuur;
+    private @Id
+    @GeneratedValue
+    Long Id;
+    final private Instant meetTijdStip;
+    final private Double temperatuur;
 
+    Temperatuur(Instant meetTijdStip, Double temperatuur) {
+        this.meetTijdStip = meetTijdStip;
+        this.temperatuur = temperatuur;
+    }
 
-    Temperatuur(Instant tijdStip, Double gemetenTemperatuur){
-        meetTijdStip = tijdStip;
-        temperatuur = gemetenTemperatuur;
+    public Long getId() {
+        return Id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Temperatuur)) {
+            return false;
+        }
+        Temperatuur temp = (Temperatuur) o;
+        return Objects.equals(this.getId(), temp.Id) && Objects.equals(this.getTemperatuur(), temp.getTemperatuur())
+                && Objects.equals(this.getMeetTijdStip(), temp.getMeetTijdStip());
+    }
+
+    public Instant getMeetTijdStip() {
+        return meetTijdStip;
+    }
+
+    public Double getTemperatuur() {
+        return temperatuur;
     }
 }
